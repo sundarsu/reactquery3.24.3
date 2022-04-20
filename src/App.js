da2066587b84a4564e132df0a265976edd462dbc
sundarsu <sundarsu@microsoft.com>
2022-04-20 14:59:50 -0400

    react-query 38.11 --> 38.32 in v3.24.3 vs. v3.24.4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {QueryClient, QueryClientProvider, useQuery} from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
+
   return (
     <div className="App">
+      <QueryClientProvider client={queryClient}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SubApp></SubApp>
       </header>
+      </QueryClientProvider>
     </div>
   );
+}
+
+function SubApp() {
+  function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+  }
+
+  async function doSomething(num)
+  {
+    await timeout(1000);
+    return [num+1, num+2, num+3];
+  }
+  
+  const {data, status} = useQuery(['hi'], () => doSomething(25));
+
+  return !status.includes('success') ? <></> : data.map((num) => <p>{num}</p>);
 }
 
 export default App;
